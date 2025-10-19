6eed98386813c685feed0b96b104b829317f45e5
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-20 00:19:43 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors"
-import cookieParser from  "cookie-parser";
-import {  userRoute } from "./routes/userRoute.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import { userRoute } from "./routes/userRoute.js";
 import { residencyRoute } from "./routes/residencyRoute.js";
-dotenv.config()
-const app=express()
 
-const PORT=process.env.PORT ||8000;
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors())
+dotenv.config();
+const app = express();
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port ${PORT}`);  
-})
- app.use("/api/User",userRoute)
- app.use("/api/residency",residencyRoute)
+const PORT = process.env.PORT || 8000;
+app.use(express.json());
+app.use(cookieParser());
+
+const corsOptions = {
+  origin: "https://real-estate-project-5snbadw5p-amitesh880s-projects.vercel.app",
+  credentials: true,
+  optionsSuccessStatus: 200,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions)); // CORS configured here
+
+app.use("/api/User", userRoute);
+app.use("/api/residency", residencyRoute);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
