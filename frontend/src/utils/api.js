7ca469533230a101a7c70b2793dbb7f30f42ef66
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-19 23:56:30 +0530

    baseurl update
    baseurl update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs"
 import { toast } from "react-toastify"
 
 export const api = axios.create({
-    baseURL: "http://localhost:3000/api"
+    baseURL: "https://real-estate-backend-imyn3riw6-amitesh880s-projects.vercel.app/api"
 })
 
 export const getAllProperties = async () => {
