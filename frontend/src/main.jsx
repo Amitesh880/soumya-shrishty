1e44b79fd10e4c5eb51f2492054101bc683e141d
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-20 01:45:21 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,7 +9,7 @@ import "@mantine/dates/styles.css";
 import 'leaflet/dist/leaflet.css';
 
 createRoot(document.getElementById('root')).render(
-    <Auth0Provider domain="dev-1u7ju166q720ebc3.us.auth0.com" clientId="gJvpPSYZ7jOP3jCMfrDaEVs037YpH4Xe" authorizationParams={{ redirect_uri:"http://localhost:5173"}}
+    <Auth0Provider domain="dev-1u7ju166q720ebc3.us.auth0.com" clientId="gJvpPSYZ7jOP3jCMfrDaEVs037YpH4Xe" authorizationParams={{ redirect_uri:"https://real-estate-project-henna-seven.vercel.app"}}
     audience="http://localhost:3000"
     scope="openid profile email"
     >
