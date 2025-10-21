d0b549422ee9dbe4766b94d26f1503e11a538889
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-21 19:39:15 +0530

    Update auth0Config.js

--- backend/config/auth0Config.js
+++ backend/config/auth0Config.js
@@ -1,13 +1,9 @@
-import { auth } from 'express-oauth2-jwt-bearer';
-
-const ISSUER_BASE_URL = "https://dev-1u7ju166q720ebc3.us.auth0.com";
-const API_IDENTIFIER = process.env.AUTH0_AUDIENCE || "http://localhost:3000";
+import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtcheck = auth({
-  audience: API_IDENTIFIER,
-  issuerBaseURL: ISSUER_BASE_URL,
+  audience: process.env.AUTH0_AUDIENCE || "http://localhost:3000",
+  issuerBaseURL: "https://dev-1u7ju166q720ebc3.us.auth0.com",
   tokenSigningAlg: "RS256",
-  jwksUri: `${ISSUER_BASE_URL}/.well-known/jwks.json` 
 });
 
 export default jwtcheck;
