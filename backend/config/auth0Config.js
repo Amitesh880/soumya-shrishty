e51cfbc2c6ff90cb391a194df0ab4b55915a5afe
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-21 20:39:32 +0530

    Update auth0Config.js

--- backend/config/auth0Config.js
+++ backend/config/auth0Config.js
@@ -5,12 +5,5 @@ const jwtcheck = auth({
   issuerBaseURL: "https://dev-1u7ju166q720ebc3.us.auth0.com",
   tokenSigningAlg: "RS256",
 });
-const logJwtErrors = (err, req, res, next) => {
-  if (err) {
-    console.error('JWT Error:', err);
-    return res.status(401).json({ message: 'Unauthorized', error: err.message });
-  }
-  next();
-};
-app.use(jwtcheck);
-app.use(logJwtErrors);
+
+export default jwtcheck;
