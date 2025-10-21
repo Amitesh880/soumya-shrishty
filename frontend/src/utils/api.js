b655a1a02d027e0aee137869bc6d36b3ca6de2f2
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-21 23:31:17 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ import { toast } from "react-toastify"
 
 export const api = axios.create({
     baseURL: "https://real-estate-backend-nine-opal.vercel.app/api",
-    withCredentials: true,
+    credentials: true,
     headers: {
         'Content-Type': 'application/json'
     }
