9b648a11062c8f543c4112d2155968f73dce6af8
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-22 23:39:18 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs"
 import { toast } from "react-toastify"
 
 export const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000/api",
+    baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
