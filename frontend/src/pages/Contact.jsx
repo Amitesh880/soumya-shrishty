6db9e790e227c96585a98d816b48d890e912b345
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-22 23:58:01 +0530

    Update Contact.jsx

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -41,7 +41,7 @@ const Contact = () => {
 
     try {
       const BASE_URL = import.meta.env.VITE_API_BASE_URL;
-      const response = await fetch(`${BASE_URL}/api/contact/message`, {
+      const response = await fetch(`${BASE_URL}/contact/message`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
