e4d669062b7e5b0966fa0561a928ef1958b414cf
Amitesh880 <amitesh.2327cse1281@kiet.edu>
2025-10-23 11:47:18 +0530

    logo

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import logo from "../assets/logo.png"
+import logo from "../assets/logosoumya.png"
 import Navbar from './Navbar'
 import { MdClose, MdMenu } from 'react-icons/md'
 import { LuUserRound } from 'react-icons/lu'
